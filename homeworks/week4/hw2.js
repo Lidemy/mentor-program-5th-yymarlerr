622e1e84c37a8ff876f7c83be369b46b2d495e88
Sonya <yymarlerr@gmail.com>
2021-05-12 21:39:26 +0800

    0512itsfinal

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,52 +1,138 @@
 const request = require('request')
 const process = require('process')
 
-if (process.argv[2] === 'list') {
-  request.get(
-    'https://lidemy-book-store.herokuapp.com/books/',
+const [, , action, id, newName] = process.argv
+
+switch (action) {
+  case 'list':
+    listBook()
+    break
+  case 'read':
+    readBook(id)
+    break
+  case 'delete':
+    deleteBook(id)
+    break
+  case 'create':
+    createBook(action, id)
+    break
+  case 'update':
+    updateBook(action, id, newName)
+    break
+  default:
+    console.log('unknown action')
+}
+
+function listBook() {
+  request(
+    'https://lidemy-book-store.herokuapp.com/books?_limit=10',
     (error, response, body) => {
-      const json = JSON.parse(body)
-      for (let i = 0; i < 20; i++) {
-        console.log(json[i].id, json[i].name)
+      if (error) {
+        console.log('error:', error)
+        return
+      }
+
+      let books
+      try {
+        books = JSON.parse(body)
+      } catch (error) {
+        console.log(error)
+        return
+      }
+
+      for (let i = 0; i < books.length; i++) {
+        console.log(books[i].id, books[i].name)
       }
     }
   )
-} else if (process.argv[2] === 'read') {
+}
+
+function readBook(id) {
   request.get(
-    `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
+    `https://lidemy-book-store.herokuapp.com/books/${id}`,
     (error, response, body) => {
-      const json = JSON.parse(body)
-      console.log(json.name)
+      if (error) {
+        console.log('error', error)
+        return
+      }
+
+      let books
+      try {
+        books = JSON.parse(body)
+      } catch (error) {
+        console.log(error)
+        return
+      }
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        console.log(books.name)
+      } else {
+        console.log('找不到相關書籍')
+      }
     }
   )
-} else if (process.argv[2] === 'delete') {
+}
+
+function deleteBook(id) {
   request.delete(
     {
-      url: `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`
-    }, (err, response, body) => {
-      console.log(body)
+      url: `https://lidemy-book-store.herokuapp.com/books/${id}`
+    }, (error, response, body) => {
+      if (error) {
+        console.log('error', error)
+        return
+      }
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        console.log('刪除成功')
+      } else {
+        console.log('刪除失敗，找不到相符 ID')
+      }
     }
   )
-} else if (process.argv[2] === 'create') {
+}
+
+function createBook(action, name) {
+  function checkId() {
+    if (id) {
+      return id
+    } else {
+      return '未輸入書名'
+    }
+  }
   request.post(
     {
       url: 'https://lidemy-book-store.herokuapp.com/books/',
       form: {
-        name: process.argv[3]
+        name: checkId()
       }
-    }, (err, response, body) => {
-      console.log(body)
+    }, (error, response, body) => {
+      if (error) {
+        console.log('error', error)
+        return
+      }
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        console.log(body)
+      }
     }
   )
-} else if (process.argv[2] === 'update') {
+}
+
+function updateBook(action, id, newName) {
   request.patch(
     {
-      url: `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
+      url: `https://lidemy-book-store.herokuapp.com/books/${id}`,
       form: {
-        name: process.argv[4]
+        name: newName
       }
-    }, (err, response, body) => {
-      console.log(body)
+    }, (error, response, body) => {
+      if (error) {
+        console.log('error', error)
+        return
+      }
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        console.log(body)
+      } else {
+        console.log('修改失敗')
+      }
     }
   )
 }
--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -5,12 +5,24 @@ const process = require('process')
 request(
   `https://restcountries.eu/rest/v2/name/${process.argv[2]}`
   , (error, response, body) => {
-    if (response.statusCode === 200) {
-      const json = JSON.parse(body)
-      console.log('國家：', json[0].name)
-      console.log('首都：', json[0].capital)
-      console.log('貨幣：', json[0].currencies[0].code)
-      console.log('國碼：', json[0].callingCodes[0])
+    if (error) {
+      console.log('error', error)
+      return
+    }
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      try {
+        const data = JSON.parse(body)
+        for (let i = 0; i < data.length; i++) {
+          console.log('============')
+          console.log(`國家：${data[i].name}`)
+          console.log(`首都：${data[i].capital}`)
+          console.log(`貨幣：${data[i].currencies[0].code}`)
+          console.log(`國碼：${data[i].callingCodes[0]}`)
+        }
+      } catch (error) {
+        console.log('error', error)
+      }
     } else {
       console.log('找不到國家資訊')
     }
