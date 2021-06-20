6b2300431ae47043f77474cfda59e39d66d0212d
Sonya <yymarlerr@gmail.com>
2021-06-20 18:31:01 +0800

    0620final

--- homeworks/week7/hw1/week7hw1.js
+++ homeworks/week7/hw1/week7hw1.js
@@ -1,44 +1,61 @@
 document.querySelector('form').addEventListener('submit',
   (e) => {
-    const selectItems = document.querySelectorAll('input')
-    function isBlank() {
-      for (let i = 0; i < selectItems.length - 2; i++) {
-        if (selectItems[i].value === '') {
-          return true
-        } else {
-          return false
-        }
-      }
-    }
+    addReminder(0, 0)
+    addReminder(1, 1)
+    addReminder(2, 2)
+    addReminder(3, 4)
+    isChecked()
 
-    if (!(selectItems[3].checked || selectItems[4].checked)) {
-      document.querySelectorAll('.form__reminder')[3].style = 'color: red'
-    }
-
-    if (selectItems[0].value === '') {
-      document.querySelectorAll('.form__reminder')[0].style = 'color: red'
-    }
-
-    if (selectItems[1].value === '') {
-      document.querySelectorAll('.form__reminder')[1].style = 'color: red'
-    }
-
-    if (selectItems[2].value === '') {
-      document.querySelectorAll('.form__reminder')[2].style = 'color: red'
-    }
-
-    if (selectItems[5].value === '') {
-      document.querySelectorAll('.form__reminder')[4].style = 'color: red'
-    }
-
-    if ((!(isBlank())) && ((selectItems[3].checked || selectItems[4].checked) === true)) {
-      let result = ''
-      for (let i = 0; i < selectItems.length - 2; i++) {
-        result += `${selectItems[i].value}/`
-      }
-      alert(result)
+    if ((!notAllIsFilledIn()) && isChecked()) {
+      alert(printContent())
     } else {
       e.preventDefault()
     }
   }
 )
+
+const selectText = document.querySelectorAll('input[type="text"]')
+const selectRadio = document.querySelectorAll('input[type="radio"]')
+const selectReminder = document.querySelectorAll('.form__reminder')
+
+function printContent() {
+  let result = ''
+  for (let i = 0; i < selectText.length - 1; i++) {
+    result += `${selectText[i].value}/ `
+  }
+  if (selectRadio[0].checked) {
+    result += `${selectRadio[0].value}/ `
+  }
+  if (selectRadio[1].checked) {
+    result += `${selectRadio[1].value}/ `
+  }
+  return result
+}
+
+function addReminder(i, j) {
+  if (selectText[i].value === '') {
+    selectReminder[j].classList.remove('hide')
+  } else {
+    selectReminder[j].classList.add('hide')
+  }
+}
+
+function notAllIsFilledIn() {
+  for (let i = 0; i < selectText.length - 2; i++) {
+    if (selectText[i].value === '') {
+      return true
+    } else {
+      return selectText[i].value === ''
+    }
+  }
+}
+
+function isChecked() {
+  if (selectRadio[0].checked || selectRadio[1].checked) {
+    selectReminder[3].classList.add('hide')
+    return true
+  } else {
+    selectReminder[3].classList.remove('hide')
+    return selectRadio[0].checked || selectRadio[1].checked
+  }
+}
